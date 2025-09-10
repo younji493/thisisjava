ba3948d935b6ba7008a5b69856885b824c12b7d3
younji493 <tooro9999@gmail.com>
2025-09-10 15:40:31 +0900

    chore: init project

---
+++ src/ch01/sec08/Hello.java
@@ -0,0 +1,8 @@
+package ch01.sec08;
+
+public class Hello {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello, Java");
+    }
+}
---
+++ src/ch01/sec10/calculator.java
@@ -0,0 +1,12 @@
+package ch01.sec10;
+
+public class calculator {
+    public static void main(String[] args) {
+        //TODO Auto-generated method stub
+        int x = 1;
+        int y = 2;
+        int result = x + y;
+        System.out.println(result);
+    }
+}
+
